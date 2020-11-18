104b6f9ff63ad4ac3b8358785e68ebd4b7d3e3b3
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2020-11-18 14:59:10 +0000

    Login refactored

--- react/src/Authentication/Login/Login.tsx
+++ react/src/Authentication/Login/Login.tsx
@@ -42,7 +42,7 @@ export const Login = ({setLoginview} : Props) => {
                 setCurrentUser(response.data);
             }
 
-        }).catch(error => {
+        }).catch(() => {
             setShowErrorMessage(true);            
         });
     }
